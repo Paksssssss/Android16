1d05ae4de439e9fec880f31ad93317b2ce746dc1
Mark Vincent B. Melgo <mbmelgo1@up.edu.ph>
2015-10-25 19:09:25 +0800

    Added Date(Wrong Conversion)

--- src/com/cmsc128/OneVault_test/AddExpense.java
+++ src/com/cmsc128/OneVault_test/AddExpense.java
@@ -1,28 +1,37 @@
 package com.cmsc128.OneVault_test;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.*;
 
 import java.sql.Date;
+import java.util.Calendar;
 
 /**
  * Created by radleyrosal on 10/22/2015.
  */
 public class AddExpense extends Activity {
-
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private int year, month, day;
     String payment_method[];
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addexpenselayout);
 
+
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
 
         payment_method = new String[3];
         payment_method[0] = "Cash";
@@ -34,6 +43,38 @@ public class AddExpense extends Activity {
                 this, android.R.layout.simple_spinner_dropdown_item, payment_method);
         s.setAdapter(adapter);
     }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "Set Date", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            year = arg1;
+            month = arg2 +1;
+            day = arg3;
+            System.out.println(year);
+            System.out.println(month);
+            System.out.println(day);
+        }
+    };
 
     public void ReturnMain(View view){
 
@@ -62,7 +103,7 @@ public class AddExpense extends Activity {
         editText = (EditText) findViewById(R.id.field_tags);
         String tags = editText.getText().toString();
 
-        Date date = new Date(20121010);
+        Date date = new Date(year,month,day);
 
         Transaction transaction = new Transaction(amount, method,date,refOrCheck,description,tax,quantity,payee,tags);
 
